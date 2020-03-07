5dd724e5b4f37c20295c5dc745931a883d31cb0b
williamcioffi <31380361+williamcioffi@users.noreply.github.com>
2020-03-07 14:08:21 -0500

    Create more_iterator_trouble.r

---
+++ more_iterator_trouble.r
@@ -0,0 +1,43 @@
+# i have a nice for loop it works
+for(i in 1:nsim) {
+  print(i)
+}
+
+# now I make it a while loop for some other reason
+while(i <= nsim) {
+  print(i)
+}
+
+# oops why is the computer hanging?
+
+
+
+###
+
+
+# the for iterator range is not re-eval'd
+# say i want to do something stupid like
+# insert missing numbers (in order) to x
+x <- c(1, 3, 4, 5, 7)
+
+for(i in 2:length(x)) {
+  if(x[i] - x[i-1] > 1) {
+    x <- c(x[1:(i-1)], x[i]+1, x[i:length(x)])
+  }
+}
+
+# oops this stops short because length(x) is only
+# eval'd at the beginning and doesn't grow
+
+# one solution use a while loop
+# is there a smarter way to do this?
+x <- c(1, 3, 4, 5, 7)
+
+i <- 2
+while(i <= length(x)) {
+  if(x[i] - x[i-1] > 1) {
+    x <- c(x[1:(i-1)], x[i-1]+1, x[i:length(x)])
+  }
+  
+  i <- i + 1
+} 
