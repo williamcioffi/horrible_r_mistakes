368796781ee5fb8aa751ad8e51e659580c19b3c9
williamcioffi <31380361+williamcioffi@users.noreply.github.com>
2020-03-07 14:10:04 -0500

    Update forgotten_iterator.R

--- forgotten_iterator.R
+++ forgotten_iterator.R
@@ -8,3 +8,22 @@ for(i in 1:nsim) {
 
 # oops where'd the counts go?
 hist(counts)
+
+
+
+### here's another one
+
+
+
+# i have a nice for loop it works
+for(i in 1:nsim) {
+  print(i)
+}
+
+# now I make it a while loop for some other reason
+i <- 1
+while(i <= nsim) {
+  print(i)
+}
+
+# oops why is the computer hanging?
