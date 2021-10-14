4bbf9d87dfc145f410d3ba24c55af1c8d7cbbf46
williamcioffi <william.cioffi@gmail.com>
2021-10-14 18:37:52 -0400

    a new mistake!

---
+++ refactoring_an_id.r
@@ -0,0 +1,39 @@
+# say you want to refactor an id based on ordering on some other parameter.
+
+# for instance you have a datatable with sightings of that id
+set.seed(311828578)
+
+sight_df <- data.frame(
+  id = sample(1:10, 100, replace = TRUE),
+  date = rnorm(100),
+  x = rnorm(100),
+  y = rnorm(100)
+)
+
+# want to refactor the ids so they go from 1:length(unique(id)) and are sorted by the value of the mean x for each individual id
+meanx <- tapply(sight_df$x, sight_df$id, mean)
+
+# make a joy division
+neworder <- order(meanx)
+
+# apply the refactoring
+sight_df$newid <- neworder[sight_df$id]
+
+# check your work
+order(tapply(sight_df$x, sight_df$newid, mean))
+#  [1]  1  7  6  2  3  9  8  4  5 10
+
+
+###
+### uh oh what went wrong?
+###
+
+# you need to do the order of the order to get the new order!
+neworder <- order(order(meanx))
+sight_df$newid <- neworder[sight_df$id]
+
+# check your work
+order(tapply(sight_df$x, sight_df$newid, mean))
+# [1]  1  2  3  4  5  6  7  8  9 10
+
+# ok!
